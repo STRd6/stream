762a1a5a435cf794c7f27eaf0d2e499d710b579e
Daniel X Moore <yahivin@gmail.com>
2013-09-23 11:31:12 -0700

    :fire_engine::pencil: Updated in browser at strd6.github.io/editor

--- master.js
+++ master.js
@@ -1,48 +1,4 @@
 (function() {
-  var Stream, emptyStream;
 
-  Stream = function(first, rest) {
-    var self;
-    if (rest == null) {
-      rest = function() {
-        return emptyStream;
-      };
-    }
-    return self = {
-      first: function() {
-        return first;
-      },
-      rest: rest,
-      get: function(n) {
-        if (n === 0) {
-          return self.first();
-        } else {
-          return rest().get(n - 1);
-        }
-      },
-      each: function(fn) {
-        fn(self.first());
-        rest().each(fn);
-        return self;
-      },
-      map: function(fn) {
-        return Stream(fn(self.first()), function() {
-          return rest().map(fn);
-        });
-      }
-    };
-  };
-
-  emptyStream = {
-    map: function() {
-      return emptyStream;
-    },
-    each: function() {
-      return emptyStream;
-    },
-    get: function() {}
-  };
-
-  module.exports = Stream;
 
 }).call(this);
